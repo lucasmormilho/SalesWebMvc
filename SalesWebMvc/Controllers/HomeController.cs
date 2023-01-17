c489f41b72fa631fd14a94b759b3c05bbcb2ee7d
lucasmormilho <56029129+lucasmormilho@users.noreply.github.com>
2023-01-17 06:49:43 -0300

    First controller and razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["Nome"] = "Lucas Star Mormilho";
 
             return View();
         }
